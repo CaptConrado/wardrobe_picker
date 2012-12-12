629fe067cc0cea7197dd9797abbc7f16f342539c
Conrad Ottey <cottey@gapintelligence.com>
2012-12-11 17:23:38 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wardrobe::Application.config.secret_token = 'd8c86f82dadc2b7991392f26fb49210af2bf59c036ea016c1080726645d1d9105c4f893b7cc0779e0e8edbbeaf2525bc891f880a97b2f7a502bb4bbf6758b33c'
